2996b2e686d24ecaedce6917aef382f2197f50a3
Gaurav Singh <104963309+Biziz123@users.noreply.github.com>
2022-12-06 10:18:03 +0530

    Updates

---
+++ precache-manifest.16b9170142b2c6626415c5fc21d93451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbbcd2895c92caac6b26",
+    "url": "/CaseChanger/static/css/main.f5885e46.chunk.css"
+  },
+  {
+    "revision": "cbbcd2895c92caac6b26",
+    "url": "/CaseChanger/static/js/main.cbbcd289.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/CaseChanger/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/CaseChanger/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "e364d6d5e4fd87b18917",
+    "url": "/CaseChanger/static/js/runtime~main.e364d6d5.js"
+  },
+  {
+    "revision": "4ae9c22618e26f6a1b3eadb64b01cf40",
+    "url": "/CaseChanger/index.html"
+  }
+];
